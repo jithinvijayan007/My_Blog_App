659f81a1d60a568e0abaf2c0dae4596ddbb05ea8
Jithin <67956333+jithinvijayan007@users.noreply.github.com>
2020-10-10 21:08:38 +0530

    Update admin.py

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,29 @@
 from django.contrib import admin
 from blogs.models import Posts
-# Register your models here.
 admin.site.register(Posts)
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
+
+
+class CustomUserAdmin(UserAdmin):
+    """Define admin model for custom User model with no username field."""
+    fieldsets = (
+        (None, {'fields': ('email','username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+admin.site.register(get_user_model(), CustomUserAdmin)
