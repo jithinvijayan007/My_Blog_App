93dc14b73daa2fe2912b16643942652a9565fd79
Jithin <67956333+jithinvijayan007@users.noreply.github.com>
2020-10-17 21:22:22 +0530

    Update views.py

--- blogs/views.py
+++ blogs/views.py
@@ -38,3 +38,30 @@ class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     def test_func(self):
         post=self.get_object()
         return self.request.user==post.author
+
+    
+    def home_screen_view(request):
+	
+	context = {}
+
+	query = ""
+	query = request.GET.get('q', '')
+	context['query'] = str(query)
+	print("home_screen_view: " + str(query))
+
+	blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
+	
+	# Pagination
+	page = request.GET.get('page', 1)
+	blog_posts_paginator = Paginator(blog_posts, BLOG_POSTS_PER_PAGE)
+
+	try:
+		blog_posts = blog_posts_paginator.page(page)
+	except PageNotAnInteger:
+		blog_posts = blog_posts_paginator.page(BLOG_POSTS_PER_PAGE)
+	except EmptyPage:
+		blog_posts = blog_posts_paginator.page(blog_posts_paginator.num_pages)
+
+	context['blog_posts'] = blog_posts
+
+	return render(request, "personal/home.html", context)
